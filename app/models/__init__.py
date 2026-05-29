a08d2c1fc86b2afc7f601296e90a61f28e322fef
emmapk <ahamefulammadu@gmail.com>
2026-05-29 06:06:40 -0700

    Fix ApiKey import name mismatch

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -7,7 +7,7 @@ from app.models.live_class import LiveClass, ClassAttendance
 from app.models.content import Book, SavedBook, BookReadProgress, Video, Note, Syllabus
 from app.models.notification import Notification
 from app.models.sia_note import SiaNote
-from app.models.api_key import APIKey
+from app.models.api_key import ApiKey, ApiUsageLog, ApiDailyUsage
 from app.models.review_report import ReviewReport
 from app.models.wallet import TeacherWallet, WalletTransaction, WithdrawalRequest
 from app.models.student_analytics import StudentLearningProfile, LessonSession
